2a680c275191b4b6a3e7d3bb86ab515d07e9aa4d
Azonos712 <nekooolay@mail.ru>
2021-01-28 16:27:36 +0300

    Test string

--- SalaryCalculation/Program.cs
+++ SalaryCalculation/Program.cs
@@ -6,7 +6,7 @@ namespace SalaryCalculation
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Nik!");
         }
     }
 }
