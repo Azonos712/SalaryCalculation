6fb59291bfafa51dd8c068a4943888146eeb3557
Azonos712 <nekooolay@mail.ru>
2021-01-29 09:51:56 +0300

    restruct project

---
+++ SalaryCalculationLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SalaryCalculationLibrary
+{
+    public class Class1
+    {
+    }
+}
--- SalaryCalculationLibrary/Model/Employee.cs
+++ SalaryCalculationLibrary/Model/Employee.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SalaryCalculation
+namespace SalaryCalculationLibrary
 {
     enum Roles
     {
