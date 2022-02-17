0bb256f7a58003d211ebcf43270acdd092e55bd9
Azonos712 <nekoooolay@mail.ru>
2022-02-17 09:58:16 +0300

    Shortening working standards naming, adding gitignore and updating project settings

--- SalaryCalculation.Library/WorkStandarts.cs
+++ SalaryCalculation.Library/WorkStandarts.cs
@@ -2,10 +2,10 @@
 {
     class WorkStandarts
     {
-        public const byte WORK_DAYS = 5;
-        public const byte WORK_WEEKS = 4;
-        public const byte HOURS_IN_WORK_DAY = 8;
-        public const byte HOURS_IN_WORK_WEEK = HOURS_IN_WORK_DAY * WORK_DAYS;
-        public const byte HOURS_IN_WORK_MONTH = HOURS_IN_WORK_WEEK * WORK_WEEKS;
+        public const byte DAYS = 5;
+        public const byte WEEKS = 4;
+        public const byte HOURS_IN_DAY = 8;
+        public const byte HOURS_IN_WEEK = HOURS_IN_DAY * DAYS;
+        public const byte HOURS_IN_MONTH = HOURS_IN_WEEK * WEEKS;
     }
 }
